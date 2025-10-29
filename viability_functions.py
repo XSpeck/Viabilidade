17a11d3030b43fdc78b5fee409168f16bf1868b2
XSpeck <leonardo.jcomservtel@gmail.com>
2025-10-29 11:04:01 -0300

    Update viability_functions.py

--- viability_functions.py
+++ viability_functions.py
@@ -176,7 +176,7 @@ def get_user_results(username: str) -> List[Dict]:
             supabase.table('viabilizacoes')
             .select('*')
             .eq('usuario', username)            
-            .or_('status.in.(aprovado,rejeitado,utp,pendente),status_predio.in.(aguardando_dados,agendado,estruturado)')
+            .or_('status.in.(aprovado,rejeitado,utp,pendente,em_auditoria),status_predio.in.(aguardando_dados,agendado,estruturado)')
             .is_('data_finalizacao', None)
             .order('data_auditoria', desc=True)
             .execute()
